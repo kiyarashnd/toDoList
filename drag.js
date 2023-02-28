d7ab4e6b8025e93097a6b1b337416fc5ea390040
Kiyarash <kiarashnadri@yahoo.com>
2023-02-28 11:52:19 +0330

    add function myf

---
+++ drag.js
@@ -0,0 +1,109 @@
+/*const div1 = document.getElementById('div1');
+div1.addEventListener('drop', drop);
+
+const para = document.getElementById('drag1');
+
+para.addEventListener('dragstart', drag);
+//dragover event specifies where the dragged data can be dropped.
+div1.addEventListener('dragover', allowDrop);
+
+
+//drag function fire when we drag element 
+function drag(ev) {
+    console.log(ev.target.id)
+    //ev.target.id is id of element that is dragged
+    ev.dataTransfer.setData("kiya", ev.target.id);
+}
+
+//this fucntion run every time that you mouse over element inside div1 elemenet
+function allowDrop(ev) {
+    ev.preventDefault();
+    console.log(1)
+    para.classList.add('drag--over')
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    para.classList.remove('drag--over')
+    //Get the dragged data with the dataTransfer.getData() method. This method will return any data that was set to the same type in the setData() method
+    let data = ev.dataTransfer.getData("kiya");
+    //this line drop document.getElementById(data) to ev.target
+    ev.target.appendChild(document.getElementById(data));
+}*/
+//moshkel koja bud?
+//vaghti function e drop run mishod va jaye 2 ta element avaz mishod dg event listener barash register nashode bud
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const box3 = document.getElementById('box3');
+const box4 = document.getElementById('box4');
+
+box1.addEventListener('dragstart', drag);
+box2.addEventListener('dragstart', drag);
+box3.addEventListener('dragstart', drag);
+box4.addEventListener('dragstart', drag);
+
+box1.addEventListener('drop', drop);
+box2.addEventListener('drop', drop);
+box3.addEventListener('drop', drop);
+box4.addEventListener('drop', drop);
+
+
+box1.addEventListener('dragover', allowDrop);
+box2.addEventListener('dragover', allowDrop);
+box3.addEventListener('dragover', allowDrop);
+box4.addEventListener('dragover', allowDrop);
+
+
+
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+
+function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+
+function drop(e) {
+    e.preventDefault();
+    // console.log(e.target)
+    clone = e.target.cloneNode(true);
+    let data = e.dataTransfer.getData("text");
+    if (clone.id !== data) {
+        let nodelist = document.getElementById("parent").childNodes;
+        for (let i = 0; i < nodelist.length; i++) {
+            if (nodelist[i].id == data) {
+                dragindex = i;
+            }
+
+        }
+
+        document.getElementById("parent").replaceChild(document.getElementById(data), e.target);
+
+        document.getElementById("parent").insertBefore(clone, document.getElementById("parent").childNodes[dragindex]);
+    }
+    const box1 = document.getElementById('box1');
+    const box2 = document.getElementById('box2');
+    const box3 = document.getElementById('box3');
+    const box4 = document.getElementById('box4');
+
+    box1.addEventListener('dragstart', drag);
+    box2.addEventListener('dragstart', drag);
+    box3.addEventListener('dragstart', drag);
+    box4.addEventListener('dragstart', drag);
+
+    box1.addEventListener('drop', drop);
+    box2.addEventListener('drop', drop);
+    box3.addEventListener('drop', drop);
+    box4.addEventListener('drop', drop);
+
+
+    box1.addEventListener('dragover', allowDrop);
+    box2.addEventListener('dragover', allowDrop);
+    box3.addEventListener('dragover', allowDrop);
+    box4.addEventListener('dragover', allowDrop);
+
+}
+
+function allowDrop(e) {
+    e.preventDefault();
+}
---
+++ tdl.js
@@ -0,0 +1,258 @@
+let inp = document.getElementById('tdl');
+window.addEventListener('load', () => inp.focus());
+
+const time = document.getElementById('time');
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
+
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+//getDay() retrun number of week
+let dayOfWeek = days[date.getDay()];
+
+time.innerHTML = `${day}/${month}/${year} ${dayOfWeek}`;
+
+const section = document.getElementById('show');
+
+const addBtn = document.getElementById('Add');
+addBtn.addEventListener('click', add);
+
+//for make empty object in first load in local storage
+if (localStorage.getItem('tdl') === null) {
+    let workObject = {};
+    let indextdl = 0;
+    localStorage.setItem('tdl', JSON.stringify(workObject))
+    localStorage.setItem('indextdl', indextdl);
+}
+
+function add() {
+    if (inp.value === '') {
+        alert('First Enter a work!');
+        inp.focus();
+    }
+    else {
+        //add work in differenet key and value in local storage as a object
+        indextdl = localStorage.getItem('indextdl')
+        workObject = JSON.parse(localStorage.getItem('tdl'));
+
+        workObject[`${indextdl}a`] = inp.value;
+
+        const divElement = document.createElement('div');
+        divElement.style.fontFamily = "'Roboto Mono', monospace";
+        //add id to new element
+        divElement.setAttribute('id', `${indextdl++}a`);
+
+        //update value of indextdl :
+        localStorage.setItem('indextdl', indextdl);
+        //set value of work for localstorage
+        localStorage.setItem('tdl', JSON.stringify(workObject));
+
+        divElement.innerHTML = `<input type="checkbox">${inp.value}<button class="delete">delete</button><button class="edit">edit</button>`
+        section.appendChild(divElement);
+
+        inp.value = '';
+
+        divElement.querySelector('input').addEventListener('change', completed);
+        divElement.querySelector('.delete').addEventListener('click', trash);
+        divElement.querySelector('.edit').addEventListener('click', editText);
+    }
+}
+
+function completed(event) {
+    let divElement = event.target.parentNode;
+
+    if (localStorage.getItem('comtdl') === null) {
+        let workObject2 = {};
+        localStorage.setItem('comtdl', JSON.stringify(workObject2))
+    }
+    //add an item to comtdl localstorge
+    let comIndexTdl = String(divElement.id);
+    let workObject2 = JSON.parse(localStorage.getItem('comtdl'));
+
+    //add compeleted item value to comtdl in locastroge 
+    workObject2[comIndexTdl] = divElement.firstElementChild.nextSibling.nodeValue;
+
+    //set id to new element 
+    divElement.setAttribute('id', comIndexTdl);
+
+    localStorage.setItem('comtdl', JSON.stringify(workObject2));
+
+    //delete item from tdl local storage from its id :
+    const workObject = JSON.parse(localStorage.getItem('tdl'));
+    delete workObject[comIndexTdl];
+    localStorage.setItem('tdl', JSON.stringify(workObject));
+
+    divElement.remove();
+
+    divElement.setAttribute("class", "overline");
+    //set compeleted item in compteletd box in html page
+    const complete = document.getElementById('completed');
+    complete.appendChild(divElement);
+
+    //for return after click on inpute check box
+    divElement.firstElementChild.addEventListener('change', returns);
+}
+
+//make a new div element and add to tdl local storage and remove from completed list and comtdl local storge :
+function returns(event) {
+    let idItem = String(event.target.parentNode.id);
+    let workObject = JSON.parse(localStorage.getItem('tdl'));
+
+    workObject[idItem] = event.target.parentNode.firstElementChild.nextSibling.nodeValue;
+    localStorage.setItem('tdl', JSON.stringify(workObject));
+
+    const divElement = document.createElement('div');
+    divElement.style.fontFamily = "'Roboto Mono', monospace";
+    divElement.setAttribute('id', idItem);
+
+    divElement.innerHTML = `<input type="checkbox">${event.target.nextSibling.nodeValue}<button class="delete">delete</button><button class="edit">edit</button>`;
+    section.appendChild(divElement);
+
+    //delete item from comtdl local storge
+    const workObject2 = JSON.parse(localStorage.getItem('comtdl'));
+    delete workObject2[idItem];
+    localStorage.setItem('comtdl', JSON.stringify(workObject2));
+    //remove elemetn from compeleted list :
+    let element = event.target.parentNode;
+    element.remove();
+
+    divElement.querySelector('input').addEventListener('change', completed);
+    divElement.querySelector('.delete').addEventListener('click', trash);
+    divElement.querySelector('.edit').addEventListener('click', editText);
+}
+
+function trash(event) {
+    let conf = confirm('You want delete this item?');
+    if (conf) {
+        //delete an item from local storage from its id :
+        const id = event.target.parentNode.id;
+        const workObject = JSON.parse(localStorage.getItem('tdl'));
+        const workObject2 = JSON.parse(localStorage.getItem('comtdl'));
+
+        //check that remove item from tdl or comtdl localstorage :
+        let flag = false;
+        for (let index in workObject) {
+            if (id === index) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            delete workObject[id];
+            localStorage.setItem('tdl', JSON.stringify(workObject));
+        }
+        else {
+            delete workObject2[id];
+            localStorage.setItem('comtdl', JSON.stringify(workObject2));
+        }
+
+        //delete an item from html page
+        let element = event.target.parentNode;
+        element.remove();
+    }
+}
+
+function editText(event) {
+    const editBtn = document.getElementById('Edit');
+
+    //for hide edit button in edit mode
+    const editButton = event.target.parentNode.lastElementChild;
+    editButton.classList.add("display");
+
+    //replace Edit instead of Add 
+    addBtn.setAttribute('class', 'display');
+    editBtn.removeAttribute('class', 'display');
+
+    //show text in input text box:
+    inp.value = event.target.parentNode.firstElementChild.nextSibling.nodeValue;
+    inp.focus();
+
+    editBtn.addEventListener('click', () => {
+        event.target.parentNode.firstElementChild.nextSibling.nodeValue = inp.value;
+        //update edited item in local storge with last id 
+        const id = event.target.parentNode.id;
+        const workObject = JSON.parse(localStorage.getItem('tdl'));
+        const workObject2 = JSON.parse(localStorage.getItem('comtdl'));
+
+        //check that edit item from tdl or comtdl localstorage :
+        let flag = false;
+        for (let index in workObject) {
+            if (id === index) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            workObject[id] = inp.value;
+            localStorage.setItem('tdl', JSON.stringify(workObject));
+        }
+        else {
+            workObject2[id] = inp.value;
+            localStorage.setItem('comtdl', JSON.stringify(workObject2));
+        }
+
+        editBtn.setAttribute('class', 'display');
+        addBtn.removeAttribute('class', 'display');
+        inp.value = '';
+        editButton.classList.remove("display");
+
+    }, { once: true });
+}
+//make works of to do list from local storage after reload
+const works = JSON.parse(localStorage.getItem('tdl'));
+
+for (let work in works)
+    afterReload(works[work], work);
+
+function afterReload(work, index) {
+    const divElement = document.createElement('div');
+    divElement.style.fontFamily = "'Roboto Mono', monospace";
+    //add id to div element from local stroge
+    divElement.setAttribute('id', index);
+
+    divElement.innerHTML = `<input type="checkbox">${work}<button class="delete">delete</button><button class="edit">edit</button>`
+    section.appendChild(divElement);
+
+    inp.value = '';
+
+    divElement.querySelector('input').addEventListener('change', completed);
+    divElement.querySelector('.delete').addEventListener('click', trash);
+    divElement.querySelector('.edit').addEventListener('click', editText);
+}
+
+//make works of compteletd to do list from comtdl local storage after reload :
+const comworks = JSON.parse(localStorage.getItem('comtdl'));
+
+for (let work2 in comworks)
+    afterReloadcom(comworks[work2], work2);
+
+function afterReloadcom(work, index) {
+    const divElement = document.createElement('div');
+
+    divElement.style.fontFamily = "'Roboto Mono', monospace";
+    divElement.setAttribute("class", "overline");
+    divElement.setAttribute('id', index);
+    divElement.setAttribute("class", "overline");
+
+    divElement.innerHTML = `<input type="checkbox">${work}<button class="delete">delete</button><button class="edit">edit</button>`
+
+    //set div element on the page :
+    document.getElementById('completed').appendChild(divElement);
+
+    divElement.querySelector('input').addEventListener('change', returns);
+    divElement.querySelector('.delete').addEventListener('click', trash);
+    divElement.querySelector('.edit').addEventListener('click', editText);
+
+    divElement.querySelector('input').checked = true;
+}
+function myf() {
+    console.log(hi)
+}
