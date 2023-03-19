f7a35ac77d0f196855634ad6fefdc816c8c07ac2
Kiyarash <kiarashnadri@yahoo.com>
2023-03-19 12:36:24 +0330

    add dark mode

--- tdl.js
+++ tdl.js
@@ -253,3 +253,21 @@ function afterReloadcom(work, index) {
 
     divElement.querySelector('input').checked = true;
 }
+
+const options = {
+    bottom: '550px', // default: '32px'
+    right: '32px', // default: '32px'
+    // left: 'unset', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true, // default: true,
+    width: '4rem',
+    height: '4rem'
+}
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
